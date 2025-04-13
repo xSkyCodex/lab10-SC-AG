b46a3688c9505f1f1144dd09baed83ba76f82364
xSkyCodex <skyler019@gmail.com>
2025-04-12 23:36:28 -0400

    modified test_calculator

--- test_calculator.py
+++ test_calculator.py
@@ -15,12 +15,15 @@ class TestCalculator(unittest.TestCase):
     # ##########################
 
     ######## Partner 1
-    # def test_multiply(self): # 3 assertions
-    #     fill in code
+    def test_multiply(self): # 3 assertions
+        self.assertEqual((5, 5), 25)
+        self.assertNotEqual((99, 99), 100)
+        self.assertEqual((12, 12), 12)
 
-    # def test_divide(self): # 3 assertions
-    #     fill in code
-    # ##########################
+    def test_divide(self): # 3 assertions
+        self.assertEqual((1, 1), 1)
+        self.assertNotEqual((10, 2), 27)
+        self.assertEqual((8, 4), 2)
 
     ######## Partner 2
     # def test_divide_by_zero(self): # 1 assertion
@@ -38,22 +41,18 @@ class TestCalculator(unittest.TestCase):
     # ##########################
     
     ######## Partner 1
-    # def test_log_invalid_argument(self): # 1 assertion
-    #     # call log function inside, example:
-    #     # with self.assertRaises(<INSERT_ERROR_TYPE>):
-    #     #     logarithm(0, 5)
-    #     fill in code
+    def test_log_invalid_argument(self): # 1 assertion
+        with self.assertRaises(ValueError):
+            logarithm(0, 7)
 
-    # def test_hypotenuse(self): # 3 assertions
-    #     fill in code
+    def test_hypotenuse(self): # 3 assertions
+        self.assertEqual((3, 4), 5)
+        self.assertNotEqual((5, 5), 100)
+        self.assertEqual((6, 7), 11)
 
-    # def test_sqrt(self): # 3 assertions
-    #     # Test for invalid argument, example:
-    #     # with self.assertRaises(<INSERT_ERROR_TYPE>):
-    #     #    square_root(NUM)
-    #     # Test basic function
-    #     fill in code
-    ##########################
+    def test_sqrt(self): # 3 assertions
+        with self.assertRaises(ZeroDivisionError):
+           square_root(-5)
 
 # Do not touch this
 if __name__ == "__main__":
