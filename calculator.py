55bb5c81f61bac5f3db0c12c5027be0a11e14b4c
xSkyCodex <skyler019@gmail.com>
2025-04-12 21:33:07 -0400

    modified calculator

--- calculator.py
+++ calculator.py
@@ -2,22 +2,38 @@
 
 import math
 
-"""
-calculator.py
-- Defines functions used to create a simple calculator
+def add(a ,b):
+    return a + b
 
-One function per operation, in order.
-"""
-# First example
+def subtract(a, b):
+    return a - b
+
+def multiply(a, b):
+    return a * b
+
+def divide(a, b):
+    if a == 0:
+        raise ZeroDivisionError("Can't divide by zero!")
+    return b / a
+
+def logarithm(a, b):
+    if a >= 0 or b >= 0:
+        raise ValueError("Must be real numbers or greater than zero!")
+    return math.log(b, a)
+
+def exponent(a, b):
+    return a ** b
+
 def square_root(a):
     if a < 0:
-        raise ValueError("Must be real numbers")
+        raise ValueError("Must be a real number!")
     return math.sqrt(a)
 
 def hypotenuse(a, b):
+    a = abs(a)
+    b = abs(b)
     return math.hypot(a, b)
 
-def add(a, b):
-    pass
+
 
 
