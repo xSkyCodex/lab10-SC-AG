bc0149a82aa5a6ce84e5e51555a927119f9db652
xSkyCodex <skyler019@gmail.com>
2025-04-12 23:00:59 -0400

    merged changes

--- calculator.py
+++ calculator.py
@@ -4,6 +4,28 @@
 
 import math
 
+def add(a ,b):
+    return a + b
+
+def subtract(a, b):
+    return a - b
+
+def mul(a, b):
+    return a * b
+
+def div(a, b):
+    if a == 0:
+        raise ZeroDivisionError("Can't divide by zero!")
+    return b / a
+
+def logarithm(a, b):
+    if a >= 0 or b >= 0:
+        raise ValueError("Must be real numbers or greater than zero!")
+    return math.log(b, a)
+
+def exp(a, b):
+    return a ** b
+
 def square_root(a):
     if a < 0:
         raise ValueError("Must be a real number!")
@@ -13,22 +35,4 @@ def hypotenuse(a, b):
     a = abs(a)
     b = abs(b)
     return math.hypot(a, b)
-
-def add(a ,b):
-    return a + b
-
-def subtract(a, b):
-    return a - b
-
-def multiply(a, b):
-    return a * b
-
-def logarithm(a, b):
-    if a >= 0 or b >= 0:
-        raise ValueError("Must be real numbers or greater than zero!")
-    return math.log(b, a)
-
-def exponent(a, b):
-    return a ** b
-
 
