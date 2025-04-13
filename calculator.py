bb1c3a4fb5a6e3fea2fae8263ab700d674609812
xSkyCodex <skyler019@gmail.com>
2025-04-12 21:11:30 -0400

    modified calculator p1

--- calculator.py
+++ calculator.py
@@ -1,5 +1,7 @@
 # https://github.com/xSkyCodex/lab10-SC-AG.git
 
+import math
+
 """
 calculator.py
 - Defines functions used to create a simple calculator
@@ -8,6 +10,27 @@ One function per operation, in order.
 """
 # First example
 def add(a, b): 
-    pass
+    return a + b
+
+def subtract(a, b):
+    return a - b
+
+def multiply(a, b):
+    return a * b
+
+def divide(a, b):
+    if a == 0:
+        raise ZeroDivisionError("Can't divide by zero!")
+    return b / a
+
+def log(a, b):
+    if a <= 0 or b <= 0:
+        raise ValueError("Must be real numbers")
+    return math.log(b, a)
+
+def exp(a, b):
+    return a ** b
+
+
 
 
