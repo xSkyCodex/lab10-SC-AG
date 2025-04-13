a2d615494f1915312792a54a99cd413ce7a8474c
xSkyCodex <skyler019@gmail.com>
2025-04-12 22:34:05 -0400

    calculator reset

--- calculator.py
+++ calculator.py
@@ -1,39 +1,3 @@
-# https://github.com/xSkyCodex/lab10-SC-AG.git
-# Partner 1: Skyler Cabrera
-# Partner 2: Andres Godoy
 
-import math
-
-def square_root(a):
-    if a < 0:
-        raise ValueError("Must be a real number!")
-    return math.sqrt(a)
-
-def hypotenuse(a, b):
-    a = abs(a)
-    b = abs(b)
-    return math.hypot(a, b)
-
-def add(a ,b):
-    return a + b
-
-def subtract(a, b):
-    return a - b
-
-def multiply(a, b):
-    return a * b
-
-def divide(a, b):
-    if a == 0:
-        raise ZeroDivisionError("Can't divide by zero!")
-    return b / a
-
-def logarithm(a, b):
-    if a >= 0 or b >= 0:
-        raise ValueError("Must be real numbers or greater than zero!")
-    return math.log(b, a)
-
-def exponent(a, b):
-    return a ** b
 
 
