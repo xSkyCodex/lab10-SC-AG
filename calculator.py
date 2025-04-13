5133be6d0fbb08648015f5d6ee3622c72883a17e
xSkyCodex <skyler019@gmail.com>
2025-04-12 22:21:26 -0400

    modified calculator.py

--- calculator.py
+++ calculator.py
@@ -1,6 +1,29 @@
 # https://github.com/xSkyCodex/lab10-SC-AG.git
+# Partner 1: Skyler Cabrera
+# Partner 2: Andres Godoy
 
+import math
 
+def add(a ,b):
+    return a + b
 
+def subtract(a, b):
+    return a - b
+
+def multiply(a, b):
+    return a * b
+
+def divide(a, b):
+    if a == 0:
+        raise ZeroDivisionError("Can't divide by zero!")
+    return b / a
+
+def logarithm(a, b):
+    if a >= 0 or b >= 0:
+        raise ValueError("Must be real numbers or greater than zero!")
+    return math.log(b, a)
+
+def exponent(a, b):
+    return a ** b
 
 
