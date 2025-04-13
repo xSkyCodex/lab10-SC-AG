9a5216b5fbcf82c6468667db5d7b78763ba792b2
Agodoy31 <casata177@gmail.com>
2025-04-12 20:31:23 -0400

    Created all functions in calculator.py

--- calculator.py
+++ calculator.py
@@ -1,6 +1,7 @@
 # https://github.com/xSkyCodex/lab10-SC-AG.git
 # Partner 1: Skyler Cabrera
 # Partner 2: Andres Godoy
+import math
 
 """
 calculator.py
@@ -10,6 +11,29 @@ One function per operation, in order.
 """
 # First example
 def add(a, b): 
-    pass
+    return a + b
 
+def subtract(a, b):
+    return a - b
+
+def multiply(a, b):
+    return a * b
+
+def divide(a, b):
+    if b == 0:
+        raise ValueError("Division by 0.")
+    return a / b
+
+def logarithm(base, argument):
+    if base <= 0 or argument <= 0:
+        raise ValueError("Base and argument must be positive numbers.")
+    return math.log(argument, base)
+
+def calculate_hypotenuse(a, b):
+    return math.sqrt(a**2 + b**2)
+
+def square_root(n):
+    if n < 0:
+                raise ValueError("Cannot calculate the square root of a negative number.")
+    return math.sqrt(n)
 
